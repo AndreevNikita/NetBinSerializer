8d8262728977a202baaa0a840c0f587ab6e0cb61
Nikita Andreev <nickandreevart@gmail.com>
2020-08-08 14:30:44 +0300

    v1.0

---
+++ NetBinSerializer/NetBinSerializer/SerializeStream.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBinSerializer
+{
+	public class SerializeStream
+	{
+		public static bool ENABLE_NOT_SERIALIZABLE_WARNINGS = false;
+
+		public long Position { get => memoryStream.Position; set => memoryStream.Position = value;}
+		public long Length { get => memoryStream.Length; }
+		MemoryStream memoryStream;
+		public BinaryReader Reader { get; private set; }
+		public BinaryWriter Writer { get; private set; }
+
+		/*
+		 *                 Work
+		 */
+		
+		public SerializeStream() {
+			memoryStream = new MemoryStream();
+			Writer = new BinaryWriter(memoryStream);
+			Reader = new BinaryReader(memoryStream);
+		}
+
+		public SerializeStream(byte[] bytes) {
+			memoryStream = new MemoryStream(bytes);
+			Writer = new BinaryWriter(memoryStream);
+			Reader = new BinaryReader(memoryStream);
+			
+		}
+
+		public byte[] getBytes() {
+			memoryStream.Flush();
+			return memoryStream.ToArray();
+		}
+
+		public long Available {
+			get {
+				return memoryStream.Length - memoryStream.Position;
+			}
+		}
+		
+
+		/*
+		 *                 Write
+		 */
+
+		public void writeInt128(UInt64 hiPart, UInt64 loPart) { 
+			write(hiPart);
+			write(loPart);
+		}
+
+		public void write(Int64 value) { Writer.Write(value); }
+
+		public void write(Int32 value) { Writer.Write(value); }
+
+		public void write(Int16 value) { Writer.Write(value); }
+		
+		public void write(sbyte value) { Writer.Write(value); }
+
+
+		public void write(UInt64 value) { Writer.Write(value); }
+
+		public void write(UInt32 value) { Writer.Write(value); }
+
+		public void write(UInt16 value) { Writer.Write(value); }
+
+		public void write(byte value) { Writer.Write(value); }
+
+
+
+		public void write(Single value) { Writer.Write(value); }
+
+		public void write(Double value) { Writer.Write(value); }
+
+		public void write(byte[] bytes) {
+			write(bytes.Length);
+			memoryStream.Write(bytes, 0, bytes.Length);
+		}
+
+		public void write(string str) {
+			Writer.Write(str);
+		}
+
+		public void write(Serializable serializable) {
+			serializable.writeToStream(this);
+		}
+
+		public void writeObject(object obj) {
+			if(ENABLE_NOT_SERIALIZABLE_WARNINGS) {
+				Console.WriteLine("Warning! {0} is not serializable", obj.GetType().Name);
+			}
+			BinaryFormatter bf = new BinaryFormatter();
+			bf.Serialize(memoryStream, obj);
+		}
+
+		public void write(object value) {
+			if(value is Int64) {
+				write((Int64)value);
+			} 
+			else if(value is Int32) {
+				write((Int32)value);
+			} 
+			else if(value is Int16) {
+				write((Int16)value);
+			} 
+			else if(value is sbyte) { 
+				write((sbyte)value);
+			}
+			else if(value is UInt64) {
+				write((UInt64)value);
+			} 
+			else if(value is UInt32) {
+				write((UInt32)value);
+			} 
+			else if(value is UInt16) {
+				write((UInt16)value);
+			} 
+			else if(value is byte) {
+				write((byte)value);
+			} 
+			else if(value is float) {
+				write((float)value);
+			} 
+			else if(value is double) {
+				write((double)value);
+			} 
+			else if(value is string) {
+				write((string)value);
+			}
+			else if(value is byte[]) {
+				write((byte[])value);
+			} else if(value is SerializeStream) {
+				write(((SerializeStream)value).getBytes());
+			} else if(value is Array) {
+				write(((Array)value).Length);
+				IEnumerable<object> arr = (IEnumerable<object>)value;
+				foreach(object obj in arr)
+					write(obj);
+			} else if(value is Serializable) {
+				write((Serializable)value);
+			}
+			else {
+				if(!Serializer.serialize(value, this))
+					writeObject(value);
+			}
+		} 
+		
+
+		/*
+		 *                 Read
+		 */
+
+		public void readInt128(out UInt64 hiPart, out UInt64 loPart) { 
+			hiPart = read<UInt64>();
+			loPart = read<UInt64>();
+		}
+
+		public Int16 readInt16() { return Reader.ReadInt16(); }
+
+		public Int32 readInt32() { return Reader.ReadInt32(); }
+
+		public Int64 readInt64() { return Reader.ReadInt64(); }
+
+		public sbyte readSByte() { return Reader.ReadSByte(); }
+
+		public UInt64 readUInt64() { return Reader.ReadUInt64(); }
+
+		public UInt32 readUInt32() { return Reader.ReadUInt32(); }
+
+		public UInt16 readUInt16() { return Reader.ReadUInt16(); }
+
+		public byte readByte() { return Reader.ReadByte(); }
+
+		public Single readFloat() { return Reader.ReadSingle(); }
+
+		public Double readDouble() { return Reader.ReadDouble(); }
+
+		public byte[] readBytes() {
+			int length = readInt32();
+			byte[] buffer = new byte[length];
+			memoryStream.Read(buffer, 0, length);
+			return buffer;
+		}
+
+		public string readString() {
+			return Reader.ReadString();
+		}
+
+		public object readObject() {
+			BinaryFormatter bf = new BinaryFormatter();
+			return bf.Deserialize(memoryStream);
+		}
+
+		public Serializable readSerializable(Type t) { 
+			return (Serializable)t.GetConstructor(new Type[] { typeof(SerializeStream) }).Invoke(new object[] { this });
+		}
+
+		public void read<T>(out T result) {
+			result = (T)read(typeof(T));
+		}
+
+		public T read<T>() {
+			return (T)read(typeof(T));
+		}
+
+		public object read(Type type) {
+			if(type == typeof(Int64)) {
+				return readInt64();
+			} 
+			else if(type == typeof(Int32)) {
+				return readInt32();
+			} 
+			else if(type == typeof(Int16)) {
+				return readInt16();
+			} 
+			else if(type == typeof(sbyte)) {
+				return readSByte();
+			} 
+			else if(type == typeof(UInt64)) {
+				return readUInt64();
+			} 
+			else if(type == typeof(UInt32)) {
+				return readUInt32();
+			} 
+			else if(type == typeof(UInt16)) {
+				return readUInt16();
+			} 
+			else if(type == typeof(byte)) {
+				return readByte();
+			} 
+			else if(type == typeof(float)) {
+				return readFloat();
+			} 
+			else if(type == typeof(double)) {
+				return readDouble();
+			} 
+			else if(type == typeof(string)) {
+				return readString();
+			}
+			else if(type == typeof(byte[])) {
+				return readBytes();
+			}
+			else if(type == typeof(SerializeStream)) {
+				return new SerializeStream(readBytes());
+			}
+			else if(type.IsArray) {
+				Type elementType = type.GetElementType();
+				int length = readInt32();
+				object[] array = new object[length];
+				
+				for(int index = 0; index < length; index++) { 
+					array[index] = read(elementType);
+				}
+				Array result = Array.CreateInstance(type, length);
+				Array.Copy(array, result, length);
+				return result;
+			} else if(typeof(Serializable).IsAssignableFrom(type)) { 
+				return readSerializable(type);
+			} else {
+				object result;
+				if(!Serializer.deserialize(this, type, out result))
+					result = readObject();
+				return result;
+			}
+		}
+
+
+
+		//Backwrite sugar
+		/*
+		 * Write next length (exclude 4 bytes per int) in bytes to position
+		 */
+		public void writeNextLength(long pos) {
+			Position = pos;
+			write((UInt32)(Length - pos - sizeof(int)));
+			Position = Length;
+		}
+
+		public long rememberAndSeek(int bytes) {
+			long nowPos = Position;
+			Position += bytes;
+			return nowPos;
+		}
+
+	}
+
+	
+
+	public interface Serializable {
+		void writeToStream(SerializeStream stream);
+	}
+}
---
+++ NetBinSerializer/NetBinSerializer/Serializer.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBinSerializer {
+
+	public class SerializeMethods {
+		public delegate void SerializeMethod(object obj, SerializeStream stream);
+		public SerializeMethod serializeMethod;
+		public delegate object DeserializeMethod(SerializeStream stream);
+		public DeserializeMethod deserializeMethod;
+
+		public SerializeMethods(SerializeMethod serializeMethod, DeserializeMethod deserializeMethod) {
+			this.serializeMethod = serializeMethod;
+			this.deserializeMethod = deserializeMethod;
+		}
+	}
+
+	public static class Serializer {
+
+		private static ConcurrentDictionary<Type, SerializeMethods> serializeMethodsMap = new ConcurrentDictionary<Type, SerializeMethods>();
+
+		static Serializer() { 
+			StandartSerializeMethods.register();
+		}
+
+		public static bool addSerializeMethods(Type type, SerializeMethods.SerializeMethod serializeMethod, SerializeMethods.DeserializeMethod deserializeMethod) {
+			return serializeMethodsMap.TryAdd(type, new SerializeMethods(serializeMethod, deserializeMethod));
+		}
+
+		public static bool serialize(object obj, SerializeStream stream) {
+			SerializeMethods serializeMethods;
+			if(serializeMethodsMap.TryGetValue(obj.GetType(), out serializeMethods)) {
+				serializeMethods.serializeMethod(obj, stream);
+				return true;
+			}
+			return false;
+		} 
+
+		public static bool deserialize(SerializeStream stream, Type type, out object result) {
+			SerializeMethods serializeMethods;
+			if(serializeMethodsMap.TryGetValue(type, out serializeMethods)) {
+				result = ((SerializeMethods)serializeMethods).deserializeMethod(stream);
+				return true;
+			}
+			result = default;
+			return false;
+		} 
+
+		public static T deserialize<T>(SerializeStream stream) {
+			return (T)((SerializeMethods)serializeMethodsMap[typeof(T)]).deserializeMethod(stream);
+		}
+
+		public static bool getSerializeMethods(Type type, out SerializeMethods methods) {
+			if(serializeMethodsMap.TryGetValue(type, out methods)) { 
+				return true;
+			//For unknown types
+			} else if(typeof(Serializable).IsAssignableFrom(type)) {
+				methods = new SerializeMethods(serializeSerializable, (SerializeStream stream) => { return stream.readSerializable(type); });
+				serializeMethodsMap[type] = methods;
+				return true;
+			} else { 
+				return false;
+			}
+		}
+
+		public static SerializeMethods getSerializeMethods(Type type) { 
+			SerializeMethods result;
+			if(serializeMethodsMap.TryGetValue(type, out result)) { 
+				return result;
+			} else { 
+				return null;
+			}
+		}
+
+		public static void serializeSerializable(object serializable, SerializeStream stream) { 
+			((Serializable)serializable).writeToStream(stream);
+		}
+
+		/*
+		public void serialize<T>(T obj, SerializeStream stream) {
+			((SerializeMethods<T>)serializeMethodsMap[typeof(T)]).serializeMethod(obj, stream);
+		} */
+	}
+
+	public static class StandartSerializeMethods {
+
+		/*				Numeric types				*/
+
+		public static void serializeInt64(object value, SerializeStream stream) { stream.write((Int64)value); }
+		public static void serializeInt32(object value, SerializeStream stream) { stream.write((Int32)value); }
+		public static void serializeInt16(object value, SerializeStream stream) { stream.write((Int16)value); }
+		public static void serializeInt8(object value, SerializeStream stream) { stream.write((sbyte)value); }
+
+
+		public static void serializeUInt64(object value, SerializeStream stream) { stream.write((UInt64)value); }
+		public static void serializeUInt32(object value, SerializeStream stream) { stream.write((UInt32)value); }
+		public static void serializeUInt16(object value, SerializeStream stream) { stream.write((UInt16)value); }
+		public static void serializeUInt8(object value, SerializeStream stream) { stream.write((byte)value); }
+
+
+
+		public static void serializeFloat(object value, SerializeStream stream) { stream.write((Single)value); }
+		public static void serializeDouble(object value, SerializeStream stream) { stream.write((Double)value); }
+
+
+
+		public static object deserializeInt64(SerializeStream stream) { return stream.readInt64(); }
+		public static object deserializeInt32(SerializeStream stream) { return stream.readInt32(); }
+		public static object deserializeInt16(SerializeStream stream) { return stream.readInt16(); }
+		public static object deserializeInt8(SerializeStream stream) { return stream.readSByte(); }
+
+
+		public static object deserializeUInt64(SerializeStream stream) { return stream.readUInt64(); }
+		public static object deserializeUInt32(SerializeStream stream) { return stream.readUInt32(); }
+		public static object deserializeUInt16(SerializeStream stream) { return stream.readUInt16(); }
+		public static object deserializeUInt8(SerializeStream stream) { return stream.readByte(); }
+
+		
+
+
+		public static object deserializeFloat(SerializeStream stream) { return stream.readFloat(); }
+		public static object deserializeDouble(SerializeStream stream) { return stream.readDouble(); }
+
+		/*				Extended types				*/
+
+		public static void serializeByteArray(object value, SerializeStream stream) { stream.write((byte[])value); }
+		public static object deserializeByteArray(SerializeStream stream) { return stream.readBytes(); }
+
+
+		public static void serializeString(object value, SerializeStream stream) { stream.write((string)value); }
+		public static object deserializeString(SerializeStream stream) { return stream.readString(); }
+
+
+
+		internal static void register() { 
+			Serializer.addSerializeMethods(typeof(Int64), serializeInt64, deserializeInt64);
+			Serializer.addSerializeMethods(typeof(Int32), serializeInt32, deserializeInt32);
+			Serializer.addSerializeMethods(typeof(Int16), serializeInt16, deserializeInt16);
+			Serializer.addSerializeMethods(typeof(sbyte), serializeInt8,  deserializeInt8);
+
+			Serializer.addSerializeMethods(typeof(UInt64), serializeUInt64, deserializeUInt64);
+			Serializer.addSerializeMethods(typeof(UInt32), serializeUInt32, deserializeUInt32);
+			Serializer.addSerializeMethods(typeof(UInt16), serializeUInt16, deserializeUInt16);
+			Serializer.addSerializeMethods(typeof(byte), serializeUInt8,  deserializeUInt8);
+
+			Serializer.addSerializeMethods(typeof(Single), serializeFloat, deserializeFloat);
+			Serializer.addSerializeMethods(typeof(Double), serializeDouble, deserializeDouble);
+
+			Serializer.addSerializeMethods(typeof(byte[]), serializeByteArray, deserializeByteArray);
+			Serializer.addSerializeMethods(typeof(string), serializeString, deserializeString);
+		}
+	}
+
+
+	
+
+}
