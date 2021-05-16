1c4bc61b800708a658c485cbf5baebd1abd92f23
AndreevNikita <nickandreevart@gmail.com>
2021-05-16 13:40:47 +0300

    + nuget package description

--- NetBinSerializer/NetBinSerializer/obj/Release/netstandard2.0/NetBinSerializer.AssemblyInfo.cs
+++ NetBinSerializer/NetBinSerializer/obj/Release/netstandard2.0/NetBinSerializer.AssemblyInfo.cs
@@ -14,11 +14,12 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nikita Andreev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Nikita Andreev")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("C# types serialization library")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetBinSerializer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetBinSerializer")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/AndreevNikita/NetBinSerializer")]
 
 // Создано классом WriteCodeFragment MSBuild.
